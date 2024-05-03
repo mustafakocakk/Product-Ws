4c248c6975bbb45c3598487fcaa07cb1aaae5889
mustafa <mustafa.akocak@ulakhaberlesme.com.tr>
2024-05-03 23:14:19 +0300

    added exception handler and generic response model

---
+++ src/main/java/com/product/ws/api/exception/GlobalErrorHandler.java
@@ -0,0 +1,27 @@
+package com.product.ws.api.exception;
+
+import com.product.ws.model.base.GenericResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+
+@ControllerAdvice
+public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({RuntimeException.class})
+    ResponseEntity<?> handleDisabledException(Exception exception, HttpServletRequest httpServletRequest) {
+        int statusCode = 400;
+        if (exception instanceof AccessDeniedException)
+            statusCode = 403;
+
+
+
+        return ResponseEntity.status(statusCode).body(
+                GenericResponse.error(exception.getMessage()));
+
+    }
+}
---
+++ src/main/java/com/product/ws/api/util/SerializationUtils.java
@@ -0,0 +1,31 @@
+package com.product.ws.api.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.hibernate.internal.util.SerializationHelper;
+
+import java.io.Serializable;
+
+public class SerializationUtils {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+
+    }
+
+    public static <T> T clone(Class<T> clazz, T dtls) {
+        T clonedObject = (T) SerializationHelper.clone((Serializable) dtls);
+        return clonedObject;
+    }
+    public static ObjectMapper getMapper(){
+        return objectMapper;
+    }
+
+}
